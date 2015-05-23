02f257af2c8e8d695667aa3e9d3f25e6411ab9ff
djarrin <dmjarrin@gmail.com>
2015-05-22 20:17:01 -0400

    gallery next button reduced in size, lightbox size is managable size

---
+++ js/fancybox-params.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+    	$(".various").fancybox({
+		maxWidth	: 800,
+		maxHeight	: 600,
+		fitToView	: false,
+		width		: '100%',
+        height      : '100%',
+        autoSize    : true,
+		closeClick	: false,
+		openEffect	: 'elastic',
+		closeEffect	: 'fade',
+        mouseWheel  : false, 
+        autoCenter  : true,
+        aspectRatio : true
+         
+        
+	});
+});
